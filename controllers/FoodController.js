73a2b2cd25dec9f46a393f650d3e1f3349328357
nishanb <nishanb7@gmail.com>
2020-10-17 08:01:52 -0700

    ADD FOOD CRUD

--- controllers/FoodController.js
+++ controllers/FoodController.js
@@ -6,20 +6,23 @@ var FoodModel = require('../models/FoodModel.js');
  * @description :: Server-side logic for managing Foods.
  */
 module.exports = {
-
     /**
      * FoodController.list()
      */
     list: function (req, res) {
-        FoodModel.find(function (err, Foods) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Food.',
-                    error: err
-                });
+        FoodModel.find(
+            { UserId: req.user_id },
+            { _id: 1, name: 1, calories: 1, intake: 1, createdAt: 1 },
+            function (err, Foods) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when getting Food.',
+                        error: err,
+                    });
+                }
+                return res.json(Foods);
             }
-            return res.json(Foods);
-        });
+        );
     },
 
     /**
@@ -27,20 +30,24 @@ module.exports = {
      */
     show: function (req, res) {
         var id = req.params.id;
-        FoodModel.findOne({_id: id}, function (err, Food) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Food.',
-                    error: err
-                });
+        FoodModel.findOne(
+            { _id: id, UserId: req.user_id },
+            { _id: 1, name: 1, calories: 1, intake: 1, createdAt: 1 },
+            function (err, Food) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Error when getting Food.',
+                        error: err,
+                    });
+                }
+                if (!Food) {
+                    return res.status(404).json({
+                        message: 'No such Food',
+                    });
+                }
+                return res.json(Food);
             }
-            if (!Food) {
-                return res.status(404).json({
-                    message: 'No such Food'
-                });
-            }
-            return res.json(Food);
-        });
+        );
     },
 
     /**
@@ -48,23 +55,24 @@ module.exports = {
      */
     create: function (req, res) {
         var Food = new FoodModel({
-			name : req.body.name,
-			calories : req.body.calories,
-			intake : req.body.intake,
-			createdAt : req.body.createdAt,
-			updatedAt : req.body.updatedAt,
-			UserId : req.body.UserId
-
+            name: req.body.name,
+            calories: req.body.calories,
+            intake: req.body.intake,
+            UserId: req.user_id,
         });
 
         Food.save(function (err, Food) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when creating Food',
-                    error: err
                 });
             }
-            return res.status(201).json(Food);
+            return res.status(201).json({
+                _id: Food.id,
+                name: Food.name,
+                calories: Food.calories,
+                intake: Food.intake,
+            });
         });
     },
 
@@ -73,35 +81,40 @@ module.exports = {
      */
     update: function (req, res) {
         var id = req.params.id;
-        FoodModel.findOne({_id: id}, function (err, Food) {
+        FoodModel.findOne({ _id: id, UserId: req.user_id }, function (
+            err,
+            Food
+        ) {
             if (err) {
                 return res.status(500).json({
                     message: 'Error when getting Food',
-                    error: err
                 });
             }
             if (!Food) {
                 return res.status(404).json({
-                    message: 'No such Food'
+                    message: 'No such Food',
                 });
             }
 
             Food.name = req.body.name ? req.body.name : Food.name;
-			Food.calories = req.body.calories ? req.body.calories : Food.calories;
-			Food.intake = req.body.intake ? req.body.intake : Food.intake;
-			Food.createdAt = req.body.createdAt ? req.body.createdAt : Food.createdAt;
-			Food.updatedAt = req.body.updatedAt ? req.body.updatedAt : Food.updatedAt;
-			Food.UserId = req.body.UserId ? req.body.UserId : Food.UserId;
-			
+            Food.calories = req.body.calories
+                ? req.body.calories
+                : Food.calories;
+            Food.intake = req.body.intake ? req.body.intake : Food.intake;
+            Food.updatedAt = Date.now();
             Food.save(function (err, Food) {
                 if (err) {
                     return res.status(500).json({
                         message: 'Error when updating Food.',
-                        error: err
                     });
                 }
 
-                return res.json(Food);
+                return res.json({
+                    _id: Food.id,
+                    name: Food.name,
+                    calories: Food.calories,
+                    intake: Food.intake,
+                });
             });
         });
     },
@@ -111,14 +124,18 @@ module.exports = {
      */
     remove: function (req, res) {
         var id = req.params.id;
-        FoodModel.findByIdAndRemove(id, function (err, Food) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the Food.',
-                    error: err
-                });
+        FoodModel.findOneAndDelete(
+            {
+                $and: [({ id: id }, { UserId: req.user_id })],
+            },
+            function (err, Food) {
+                if (err || Food == null) {
+                    return res.status(500).json({
+                        message: 'Error when deleting the Food.',
+                    });
+                }
+                return res.status(200).json({ message: 'Food is deleted' });
             }
-            return res.status(204).json();
-        });
-    }
+        );
+    },
 };
